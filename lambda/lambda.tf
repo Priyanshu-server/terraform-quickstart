acfe6841e4a522c357009120ee56326a5f4b45ad
Default Name <default@example.com>
2025-11-01 16:11:34 +0530

    Added environment into lambda

--- lambda/lambda.tf
+++ lambda/lambda.tf
@@ -9,6 +9,12 @@ resource "aws_lambda_function" "lambda_one" {
   s3_bucket = var.lambda_config.lambda_s3_bucket
   s3_key    = "lambda/lambda_one_function.zip" # lambda/(zip name)
 
+  environment {
+    variables = {
+      aws_region = var.main_config.region
+    }
+  }
+
   tags = merge(
     var.main_config.tags,
     {
